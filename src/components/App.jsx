bfefb04ac1c95fbed93fd9b238db0aeb3fabf092
Hanaf Othmane <ohanaf@users.noreply.github.com>
2021-01-23 13:42:36 -0500

    Hover button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+	const [headingText, setHeadingText] = useState("Hello");
+	const [isOver, setisOver] = useState(false);
+
+	function handleCllick() {
+		setHeadingText("Submitted");
+	}
+
+	function handleOver() {
+		setisOver(true);
+	}
+
+	function handleOut() {
+		setisOver(false);
+	}
+
+	return (
+		<div className="container">
+			<h1>{headingText}</h1>
+			<input type="text" placeholder="What's your name?" />
+			<button
+				style={{ backgroundColor: isOver ? "black" : "white" }}
+				onClick={handleCllick}
+				onMouseOver={handleOver}
+				onMouseOut={handleOut}>
+				Submit
+			</button>
+		</div>
+	);
 }
 
 export default App;
